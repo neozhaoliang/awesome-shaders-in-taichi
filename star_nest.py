e4149ad214bc21c60611254054fe3e1326b854f9
Zhao Liang <mathzhaoliang@gmail.com>
2022-05-18 21:31:16 +0800

    Update star_nest.py

--- star_nest.py
+++ star_nest.py
@@ -1,10 +1,15 @@
 # https://www.shadertoy.com/view/XlfGRj
+import time
 import taichi as ti
-from taichi.math import *
+import taichi.math as tm
 
 ti.init(arch=ti.gpu)
 
 W, H = 800, 640
+iResolution = tm.vec2(W, H)
+iTime = ti.field(ti.f32, shape=())
+iMouse = ti.Vector.field(2, ti.f32, shape=())
+
 img = ti.Vector.field(3, ti.f32, shape=(W, H))
 
 iterations = 17
@@ -23,35 +28,41 @@ distfading = 0.730
 saturation = 0.850
 
 
-@ti.kernel
-def step(t: float, mx: float, my: float):
-    for i, j in img:
-        uv = vec2(i, j) / vec2(W, H) - 0.5
-        uv.y *= H / W
-        dir = vec3(uv * zoom, 1)
-        time = t * speed + 0.25
+def init():
+    iTime[None] = 0.0
+    iMouse[None] = (0, 0)
+    return time.perf_counter()
 
-        a1 = 0.5 + mx * 2
-        a2 = 0.8 + my * 2
-        R1 = rot2(-a1)
-        R2 = rot2(-a2)
+
+@ti.kernel
+def step():
+    for i, j in img:
+        uv = tm.vec2(i, j) / iResolution - 0.5
+        uv.y *= H / W
+        dir = tm.vec3(uv * zoom, 1)
+        time = iTime[None] * speed + 0.25
+
+        a1 = 0.5 + iMouse[None].x * 2
+        a2 = 0.8 + iMouse[None].y * 2
+        R1 = tm.rot2(-a1)
+        R2 = tm.rot2(-a2)
         dir.xz = R1 @ dir.xz
         dir.xy = R2 @ dir.xy
-        fr = vec3(1, 0.5, 0.5)
-        fr += vec3(2*time, time, -2)
+        fr = tm.vec3(1, 0.5, 0.5)
+        fr += tm.vec3(2*time, time, -2)
         fr.xz = R1 @ fr.xz
         fr.xy = R2 @ fr.xy
 
         s, fade = 0.1, 1.0
-        v = vec3(0)
+        v = tm.vec3(0)
         for r in range(volsteps):
             p = fr + s * dir * 0.5
-            p = abs(vec3(tile) - mod(p, vec3(tile * 2)))
+            p = abs(tm.vec3(tile) - tm.mod(p, tm.vec3(tile * 2)))
             pa = a = 0.0
             for _ in range(iterations):
-                p = abs(p) / dot(p, p) - formuparam
-                a += abs(length(p) - pa)
-                pa = length(p)
+                p = abs(p) / tm.dot(p, p) - formuparam
+                a += abs(tm.length(p) - pa)
+                pa = tm.length(p)
 
             dm = max(0, darkmatter - a * a * 0.001)
             a *= a*a
@@ -59,22 +70,22 @@ def step(t: float, mx: float, my: float):
                 fade *= 1 - dm
 
             v += fade
-            v += vec3(s, s*s, s*s*s*s) * a * fade * brightness
+            v += tm.vec3(s, s*s, s*s*s*s) * a * fade * brightness
             fade *= distfading
             s += stepsize
 
-        color = mix(vec3(length(v)), v, saturation)
+        color = tm.mix(tm.vec3(tm.length(v)), v, saturation)
         img[i, j] = color * 0.01
 
 
 def main():
+    t0 = init()
     gui = ti.GUI('Star Nest', res=(W, H))
-    t = 0.0
     while gui.running:
-        mouse_x = mouse_y = 0.0
         gui.get_event(ti.GUI.PRESS)
         if gui.is_pressed(ti.GUI.LMB):
             mouse_x, mouse_y = gui.get_cursor_pos()
+            iMouse[None] = tm.vec2(mouse_x, mouse_y) * iResolution
 
         if gui.is_pressed(ti.GUI.ESCAPE):
             gui.running = False
@@ -83,8 +94,8 @@ def main():
             gui.set_image(img)
             gui.show('screenshot.png')
 
-        step(t, mouse_x, mouse_y)
-        t += 0.01
+        step()
+        iTime[None] = time.perf_counter() - t0
         gui.set_image(img)
         gui.show()
 
