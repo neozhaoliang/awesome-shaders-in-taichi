e86f72e6027733e5600c04e78a31d4315aa8fea6
Zhao Liang <mathzhaoliang@gmail.com>
2022-05-23 19:11:43 +0800

    Update seascape.py

--- seascape.py
+++ seascape.py
@@ -12,10 +12,10 @@ ti.init(arch=ti.gpu)
 
 W, H = 800, 640
 iResolution = tm.vec2(W, H)
-iTime = ti.field(ti.f32, shape=())
-iMouse = ti.Vector.field(2, ti.f32, shape=())
+iTime = ti.field(float, shape=())
+iMouse = ti.Vector.field(2, float, shape=())
 
-img = ti.Vector.field(3, ti.f32, shape=(W, H))
+img = ti.Vector.field(3, float, shape=(W, H))
 
 NUM_STEPS = 8
 EPSILON = 1e-3
